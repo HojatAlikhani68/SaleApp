0df43bea17e974e111484e56a4c062366556d9a2
Hojat Alikhani <h.alikhani@iho.co>
2024-10-05 14:42:34 +0330

    Commit Product Categories

---
+++ Sale.Domain/Entities/Base/BaseDocumentEntity.cs
@@ -0,0 +1,13 @@
+﻿namespace Sale.Domain.Entities.Base
+{
+    public abstract class BaseDocumentEntity<TKey> : BaseEntity<TKey>
+    {
+        public DateTime DateTime { get; set; }
+        public int UserInsert { get; set; } = 0;
+        public int? UserUpdate { get; set; }
+        public DateTime InsertDate { get; set; }= DateTime.Now;
+        public DateTime? UpdateDate { get; set; }
+        public bool IsActive { get; set; }
+
+    }
+}
---
+++ Sale.Domain/Entities/Products/ProductCategory.cs
@@ -0,0 +1,18 @@
+﻿using Sale.Domain.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sale.Domain.Entities.Products
+{
+    public class ProductCategory:BaseEntity<int>
+    {
+        public required string Name { get; set; }
+        public string? Description { get; set; }
+        public int ParentId { get; set; }
+        public bool IsActive { get; set; }
+        public IList<ProductCategory> ProductCategories { get; set; } = [];
+    }
+}
