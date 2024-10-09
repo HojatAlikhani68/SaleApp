0e971f22b91c700d5a6cb4b43f334b164e0a29f5
Hojat Alikhani <h.alikhani@iho.co>
2024-10-09 15:25:14 +0330

    Commit

--- Sale.Application/Extentions/ConversionExtensions.cs
+++ Sale.Application/Extentions/ConversionExtensions.cs
@@ -52,5 +52,43 @@
 
             return destinationList;
         }
+        public static TDestination ConvertTo<TDestination>(this object source)
+        {
+            if (source == null)
+            {
+                throw new ArgumentNullException(nameof(source), "Source cannot be null");
+            }
+
+            var destination = Activator.CreateInstance<TDestination>();
+
+            foreach (var sourceProperty in source.GetType().GetProperties())
+            {
+                var destinationProperty = typeof(TDestination).GetProperty(sourceProperty.Name);
+                if (destinationProperty != null && destinationProperty.CanWrite)
+                {
+                    var value = sourceProperty.GetValue(source);
+                    destinationProperty.SetValue(destination, value);
+                }
+            }
+
+            return destination;
+        }
+        public static List<TDestination> ConvertToD<TDestination>(this IEnumerable<object> sourceList)
+        {
+            if (sourceList == null)
+            {
+                throw new ArgumentNullException(nameof(sourceList), "Source list cannot be null");
+            }
+
+            var destinationList = new List<TDestination>();
+
+            foreach (var source in sourceList)
+            {
+                var destination = source.ConvertToDynamic<TDestination>();
+                destinationList.Add(destination);
+            }
+
+            return destinationList;
+        }
     }
 }
